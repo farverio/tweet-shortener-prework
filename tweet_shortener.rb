bd1bbfefa344d8ae8ec0c54fed96442e120b1d86
Stephen F <stephen@farver.io>
2018-05-30 21:00:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
 # Write your code here.
+
+def dictionary
+  dictionary =
+    {
+      :"hello" => 'hi',
+      :"to" => '2',
+      :"two" => '2',
+      :"too" => '2', 
+      :"for" => '4',
+      :"four" => '4',
+      :'be' => 'b',
+      :'you' => 'u',
+      :"at" => "@", 
+      :"and" => "&"
+    }
+end
+
+def word_substituter(tweet_string)
+  tweet_arr = tweet_string.split(" ")
+  
+  replaced_arr = tweet_arr.map.with_index do |word, i|
+    puts dictionary[word]
+    if dictionary.has_key?(word)
+      tweet_arr[i] = dictionary[word]
+    else
+      word
+    end
+  end
+  
+  replaced_arr.join(" ")
+      
+  #input: String
+    #convert to Array
+    #check for match
+    #replace with sub 
+    #convert to String
+  #output: shortened
+end
+
+def bulk_tweet_shortener(tweet_array)
+  #input: arr of tweet strings
+    #pass to word_substituter
+    #replace value with new value
+  #output: shortened array
+end
