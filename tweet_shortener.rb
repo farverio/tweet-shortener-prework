c1d656bd579e689dce13b1b2d3fab3140b19fd4b
Stephen F <stephen@farver.io>
2018-05-30 21:01:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ def word_substituter(tweet_string)
   tweet_arr = tweet_string.split(" ")
   
   replaced_arr = tweet_arr.map.with_index do |word, i|
-    puts dictionary[:word]
+    puts dictionary[:hello]
     if dictionary.has_key?(word)
       tweet_arr[i] = dictionary[word]
     else
